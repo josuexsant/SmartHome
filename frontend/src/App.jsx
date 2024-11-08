5128cfb11e4f4770f3bc805d844ab4ce0a6c67a7
Josue Santamaria <josuexsanta@gmail.com>
2024-11-07 22:06:21 -0600

    mqtt configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
+import { publish } from "./helpers/client";
 
 function App() {
+
+  const handleClick = () => {
+    publish("Encenddido");
+  };
+
   return (
     <>
       <h1>SmartHome</h1>
-      <h4>Me da gusto verte de nuevo</h4><hr /><br /><br /><br />
+      <h4>Me da gusto verte de nuevo</h4>
+      <hr />
+      <br />
+      <br />
+      <br />
       <h2>Proximamente...</h2>
+      <button onClick={handleClick}>Encender</button>
     </>
   );
 }
---
+++ frontend/src/helpers/client.js
@@ -0,0 +1,55 @@
+import mqtt from "mqtt";
+
+// var client = new Paho.MQTT.Client("broker.emqx.io", 8084, clientId);
+const client = mqtt.connect("ws://broker.emqx.io:8083/mqtt");
+const topic = "cesar";
+
+/**
+ *  function onConnect() {
+      console.log("Conexion exitosa...");
+      client.subscribe("torneo");
+      console.log("Suscrito al tópico 'torneo'");
+    }             
+ */
+client.on("connect", () => {
+  console.log("Conexión exitosa");
+  client.subscribe(topic);
+  console.log("Subscrito al tópico: " + topic);
+});
+
+/**
+ *  function doFail(e) {
+      console.log("Fallo en la conexión: " + e.errorMessage);
+    }
+ */
+client.on("error", (e) => {
+  console.log("Fallo en la conexión: " + e.errorMessage);
+});
+
+/**
+ *  function onMessageArrived(message) {
+      console.log("Un mensaje ha llegado: " + message.payloadString);
+      var messagesDiv = document.getElementById("messages");
+      messagesDiv.innerHTML = "";
+      var newMessage = document.createElement("p");
+      newMessage.textContent = message.payloadString;
+      messagesDiv.appendChild(newMessage);
+    }
+ */
+client.on("message", (topic, message) => {
+  console.log(message.toString());
+});
+
+/**
+ *  function command(value) {
+      console.log(value);
+      var message = new Paho.MQTT.Message(value.toString());
+      message.destinationName = "torneo";
+      client.send(message);
+    }
+ */
+export const publish = (message) => {
+  client.publish(topic, message);
+};
+
+export default publish;
