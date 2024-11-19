f5142a86e9b3f19dbe1517148eb55f707f12961b
cexs <cesar.serafin@outlook.com>
2024-11-19 09:38:50 -0600

    Configuracion esp32

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -2,23 +2,23 @@
 #include "conexion.h"
 
 // Asignación de pines de la ESP32
-int lightBedroom = 2; //Led
-int ceilingBedroom = 0; //Motor
-int sensorBedroom = 12; //Dallas
-int airBedroom = 0; //Ventilador
-int lightBathroom = 13; //Led
-int servoBathroom =0 ; //Servo
-int lightKitchen = 14;  //Led
-int servoKitchen = 0; //Servo
-int lightLivingRoom = 0; //Led
-int ceilingLivingRoom = 0; //Motor
-int sensorLivingRoom = 12; //Dallas
-int airLivingRoom = 0; //Ventilador
-int displayTV = 0; //Pantalla
-int servoDoor = 0; //Servo
-int sensorBell = 0; // Sensor proximidad
+int lightBedroom = 2; // Led
+int ceilingBedroom = 0; // Motor
+int sensorBedroom = 12; // Dallas
+int airBedroom = 0; // Ventilador
+int lightBathroom = 13; // Led
+int servoBathroom = 0; // Servo
+int lightKitchen = 14;  // Led
+int servoKitchen = 0; // Servo
+int lightLivingRoom = 0; // Led
+int ceilingLivingRoom = 0; // Motor
+int sensorLivingRoom = 12; // Dallas
+int airLivingRoom = 0; // Ventilador
+int displayTV = 0; // Pantalla
+int servoDoor = 0; // Servo
+int sensorBell = 0; // Sensor de proximidad
 
-// Asignacion memoria para los sensores
+// Asignación de memoria para los sensores
 int bedRoomTemp = 6;
 int livingRoomTemp = 3;
 int bell = 2;
@@ -43,7 +43,7 @@ void updateData()
   payload += "}";
 
   // Publicar el mensaje en el tópico MQTT
-  client.publish("cesar", payload.c_str());
+  client.publish("yuli", payload.c_str());
 }
 
 void loop()
